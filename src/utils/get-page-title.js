6b763cdd423b918be9af8b749594c87256bf6d1d
heNryo3o <18661139072@163.com>
2019-10-21 17:57:26 +0800

    1

--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,6 +1,6 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || '大学士管理后台'
+const title = defaultSettings.title || '江苏乐众管理后台'
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
