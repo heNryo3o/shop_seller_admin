eeb277958d50932f80e503925705d7a7a13ffb4d
henryo3o <18661139072@163.com>
2019-10-25 18:09:37 +0800

    1

--- src/views/chat/chat.vue
+++ src/views/chat/chat.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="height: 100%;width: 100%;text-align: center;">
-    <iframe :src="'http://localhost:8080/#/?username='+username" style="width: 350px;height: 600px;margin-top: 40px;"></iframe>
+    <iframe :src="'http://chat.jiangsulezhong.com/#/?username='+username" style="width: 350px;height: 600px;margin-top: 40px;"></iframe>
   </div>
 </template>
 
